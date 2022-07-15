36d08d379524eb2513153c7182aa9145c3430d5b
ᴀʙʜɪᴍᴀɴʏᴜ ᴀʜᴜᴊᴀ <77237092+abhimanyuahuja@users.noreply.github.com>
2022-07-15 14:15:54 +0530

    Time: 485 ms (58.97%), Space: 145.3 MB (31.25%) - LeetHub

--- 2049-count-nodes-with-the-highest-score/2049-count-nodes-with-the-highest-score.cpp
+++ 2049-count-nodes-with-the-highest-score/2049-count-nodes-with-the-highest-score.cpp
@@ -3,6 +3,7 @@ public:
     #define ll long long
     int dfs(int curr  , int par  , int * child , vector<int> gr[],int n,ll * ans){
         child[curr] = 1;
+        
         int left=-1,right=-1;
         for(auto x : gr[curr]){
             if(x!=par){
@@ -10,9 +11,17 @@ public:
                 else if(right==-1) right = dfs(x,curr,child,gr,n,ans);
             }
         }
-        child[curr] += ((left!=-1)?left:0) + ((right!=-1)?right:0);
-        // cout<<curr<<" "<<left<<" "<<right<<"\n";
-        ans[curr] = 1ll*((left!=-1)?left:1)*((right!=-1)?right:1)*((n-1-((left!=-1)?left:0)-((right!=-1)?right:0))>0?(n-1-((left!=-1)?left:0)-((right!=-1)?right:0)):1);
+        int leftchild = ((left!=-1)?left:0);
+        int rightchild = ((right!=-1)?right:0);
+        
+        child[curr] +=  leftchild + rightchild;
+        
+        int score_of_left = (left!=-1)?left:1; // val of left
+        int score_of_right = (right!=-1)?right:1; // val of right
+        int score_of_parent = max(n-1-leftchild-rightchild , 1); //val of parent subtree 
+        
+        
+        ans[curr] = 1ll*score_of_left*score_of_right*score_of_parent;
         return child[curr];
     }
     int countHighestScoreNodes(vector<int>& parents) {
@@ -23,17 +32,25 @@ public:
             gr[i].push_back(parents[i]);
             gr[parents[i]].push_back(i);
         }
-        int child[n];
-        ll ans[n];
+        int child[n]; // child array to calculate the children of nodes
+        ll ans[n]; // calculating score of each node
+        
+        // init them with 0
         memset(ans,0,sizeof(ans));
         memset(child,0,sizeof(child));
+        
+        // dfs
         child[0] = dfs(0,-1,child,gr,n,ans);
+        
+        
+        // maximum score in tree
         ll maxi = INT_MIN;
-        // cout<<"\n ans : "; 
         for(int i = 0 ; i < n; i++){
             if(ans[i]>maxi) maxi = ans[i];
         }
-        // cout<<"\n";
+        
+        
+        // calculating count of the maximum score
         int res = 0;
         for(int i = 0 ; i < n ; i++){
             if(ans[i]==maxi) res++;
