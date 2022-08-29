64ccaf1925d1c752544d8689b1136684dfa1d14d
ᴀʙʜɪᴍᴀɴʏᴜ ᴀʜᴜᴊᴀ <77237092+abhimanyuahuja@users.noreply.github.com>
2022-08-30 00:39:36 +0530

    Time: 457 ms (5.01%), Space: 7.9 MB (51.03%) - LeetHub

---
+++ 76-minimum-window-substring/76-minimum-window-substring.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    bool valid(map<char,int> &ref , map<char,int> &win){
+        for(auto x : ref){
+            if(win[x.first] < ref[x.first]){
+                return false;
+            }
+        }
+        return true;
+    }
+    string minWindow(string s, string t) {
+        map<char,int> ref;
+        for(auto x : t){
+            ref[x]++;
+        }
+        map<char,int> win;
+        
+        int i =0 , j = 0;
+        int n = s.length();
+        int mini = INT_MAX;
+        pair<int,int> p = {-1,-1};
+        while(j<n){
+            if(ref.count(s[j])) win[s[j]]++;
+            while(valid(ref,win) and i <= j){
+                if(ref.count(s[j])) win[s[i]]--;
+                if(j-i+1 < mini){
+                    mini = j-i+1;
+                    p = {i,j};
+                }
+                i++;
+            }
+            j++;
+        }
+        i--;
+        if(i<0) return "";
+        j--;
+        if(j-i+1 < mini){
+            mini = j-i+1;
+            p = {i,j};
+        }
+        if(mini == INT_MAX) return "";
+        string ans = s.substr(p.first,p.second-p.first+1);
+        return ans;
+    }
+};
